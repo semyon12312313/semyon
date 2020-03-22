d2904b1ace0469d822fa0670fde804ec030d196f
wert <dfgh@gmail.com>
2020-03-22 12:47:41 +0500

    first

---
+++ main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int wsquere (int a)
+{
+  int b=10;
+          int c;
+          for (b-1=b; b>0; )
+              if( b==0)
+                  cout <<c;
+   return c ;
+
+}
+int main()
+{
+
+
+
+
+    {cout <<  wsquere(2);
+}
+//{cout <<" ";
+//    }
+//int min=1;
+//int max=5;
+//srand(time(0));
+//int f;
+//int r= rand()%(max-min+1)+min;
+//for(int i=0; i<3; i++;)
+//{cin>> f;
+//    if (f==r)
+//    cout << "win ";
+//        else
+//    cout << "lost ";
+//}
+
+
+
+
+//    int min=0;
+//    int max=2130000;
+//     srand(time(0));
+//int r= rand()%(max-min+1)+min;
+// cout << r  ;
+// cout<< " ";
+
+
+
+//    int n=124554;
+//    int i=2;
+//    while(n%i!=0)
+//        i++;
+//        {cout<< i ;}
+//    {cout<< " " ;}
+
+
+//   int n=1904;
+//   for (int i=2; i<=n/2; i++)
+//{if(n%i!=0)
+//           continue;
+//           else
+//       {cout<< i ;}
+//       {cout<< " " ;} }
+//     int n=1010123;
+//     while (!(n>99999 or n<1000000))
+//     int n;
+//             {cin >>n;}
+//        {
+//     int n1;
+//     int s1;
+//     int s2;
+//     s1=n%10+(n/10)%10+(n/100)%10;
+//     n1=n/1000;
+//     s2=n1%10+(n1/10)%10+(n1/100)%10;
+//  if(s1==s2)
+//  {cout <<"lucky ";}
+//  else
+//     { cout << "no ";
+//  }}
+
+//    int n=123;
+//  int s;
+
+//    while (n!=0)
+//    { s+=n%10;
+//     n=n/10;}
+
+//    {cout <<s;
+//    }
+//    int s=0;
+// int n;
+
+//    while(n!=0)
+//    {cin >> n;
+//      s+=n;}
+//     {cout <<s; }
+
+//int n=10;
+//int f=1;
+//int i=2;
+//while(i<=n)
+//{    f+=i;
+//    i++;}
+//{cout <<f;}
+
+
+//int n=3;
+
+//    for(int i=0; i<n; i++)
+//{
+//        for(int j=0; j<n+i-3; j++)
+//        {cout <<" ";}
+//        for(int k=0; k<n-i; k++)
+//{cout <<"*";}
+//          cout <<"\n";}
+//    int b=1;
+//    int i;
+//    int j;
+//    for (int i=0; i<3;  i++ ) {
+//    {
+//            for (int j=0; j=3; j++ )
+//                {cout <<"*";}
+//            cout <<"\n"   ;
+//    }
+//}
+//    int n=10;
+//    int a = 1;
+//    int b = 1;
+//    int c = 0;
+//    {
+//        cout << a <<" ";}
+//    {
+//    cout << b <<" ";}
+
+//            for (int i=2; i<n; i++ )
+//            {if (c=a+b);
+//                a=b;
+//                   b=c;
+//                cout << c <<" ";}
+
+//    int n=10;
+//             for (int i = 2 ;i<=n/2; i++)
+//            {if (n%==0)
+//            cout << i;}
+ return 0;
+ }
